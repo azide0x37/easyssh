0346fc65af40e4c912f7b16c9cbc35b2331a18c8
Alexander Templeton <blakmatr@gmail.com>
2017-10-16 18:28:20 -0500

    adding all the weak ciphers I can think of

--- easyssh.go
+++ easyssh.go
@@ -76,7 +76,7 @@ func (ssh_conf *MakeConfig) connect() (*ssh.Session, error) {
 	}
 
 	config := &ssh.Config{
-		Ciphers: "aes256-cbc",
+		Ciphers: ["aes128-cbc", "3des-cbc", "blowfish-cbc", "cast128-cbc", "arcfour", "aes192-cbc", "aes256-cbc", "aes128-ctr", "aes192-ctr", "aes256-ctr", "aes128-gcm@openssh.com", "arcfour256", "arcfour128"]
 	}
 
 	clientConfig := &ssh.ClientConfig{
